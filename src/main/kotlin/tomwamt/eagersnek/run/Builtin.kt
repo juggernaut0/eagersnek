6b5323e20984287356be9ae4dc5e6dff348a0b8d
Tom Warner <tomwamt@gmail.com>
2018-02-04 22:37:36 -0700

    Some string functions

--- src/main/kotlin/tomwamt/eagersnek/run/Builtin.kt
+++ src/main/kotlin/tomwamt/eagersnek/run/Builtin.kt
@@ -25,6 +25,8 @@ object Builtin {
         val trueObj = ns.bindings[True.name]!!
         val falseObj = ns.bindings[False.name]!!
 
+        ns.subnames["String"] = makeStringNamespace()
+
         ns.bindings["println"] = object : FunctionObject(1) {
             override fun call(int: Interpreter) {
                 val obj = int.execStack.pop()
@@ -69,8 +71,7 @@ object Builtin {
 
         ns.bindings["to_num"] = object : FunctionObject(1) {
             override fun call(int: Interpreter) {
-                val str = int.execStack.pop()
-                        .let { it as? StringObject ?: throw InterpreterException("Expected a String, got ${it.type.name}") }
+                val str = int.execStack.pop().cast<StringObject>()
 
                 try {
                     int.execStack.push(NumberObject(str.value.toDouble()))
@@ -83,23 +84,81 @@ object Builtin {
         return ns
     }
 
+    private fun makeStringNamespace(): Namespace {
+        val ns = Namespace(true)
+
+        ns.bindings["concat"] = object : FunctionObject(2) {
+            override fun call(int: Interpreter) {
+                val s1 = int.popString()
+                val s2 = int.popString()
+
+                int.execStack.push(StringObject(s1 + s2))
+            }
+        }
+
+        ns.bindings["char_at"] = object : FunctionObject(2) {
+            override fun call(int: Interpreter) {
+                val i = int.popInt()
+                val str = int.popString()
+
+                val ch = str[i].toString()
+                int.execStack.push(StringObject(ch))
+            }
+        }
+
+        ns.bindings["length"] = object : FunctionObject(1) {
+            override fun call(int: Interpreter) {
+                val str = int.execStack.pop().cast<StringObject>()
+                int.execStack.push(NumberObject(str.value.length.toDouble()))
+            }
+        }
+
+        ns.bindings["substr"] = object : FunctionObject(3) {
+            override fun call(int: Interpreter) {
+                val start = int.popInt()
+                val end = int.popInt()
+                val str = int.popString()
+
+                int.execStack.push(StringObject(str.substring(start, end)))
+            }
+        }
+
+        return ns
+    }
+
     private class BinaryOp(private val op: (Double, Double) -> Double) : FunctionObject(2) {
         override fun call(int: Interpreter) {
-            val n1 = int.execStack.pop() as? NumberObject ?: throw InterpreterException("Expected a number")
-            val n2 = int.execStack.pop() as? NumberObject ?: throw InterpreterException("Expected a number")
+            val n1 = int.popNumber()
+            val n2 = int.popNumber()
 
-            int.execStack.push(NumberObject(op(n1.value, n2.value)))
+            int.execStack.push(NumberObject(op(n1, n2)))
         }
     }
 
     private fun stringify(obj: RuntimeObject): String {
         return when (obj) {
-            is NumberObject -> if (obj.value - obj.value.toInt() == 0.0) obj.value.toInt().toString() else obj.value.toString()
+            is NumberObject -> if (obj.value.isInt()) obj.value.toInt().toString() else obj.value.toString()
             is StringObject -> obj.value
             is CaseObject -> "(${obj.type.name}${obj.data.joinToString("") { " ${stringify(it)}" }})"
             is SingletonObject -> obj.type.name
             is FunctionObject -> "<${obj.javaClass.name}>"
             else -> obj.toString()
         }
+    }
+
+    private fun Double.isInt() = this - toInt() == 0.0
+
+    private inline fun <reified T : RuntimeObject> RuntimeObject.cast(): T {
+        return this as? T ?: throw InterpreterException("Expected a ${T::class.java.simpleName}, got ${javaClass.simpleName}")
+    }
+
+    private fun Interpreter.popNumber() = execStack.pop().cast<NumberObject>().value
+    private fun Interpreter.popString() = execStack.pop().cast<StringObject>().value
+    private fun Interpreter.popInt(): Int {
+        return execStack.pop()
+                .cast<NumberObject>().value
+                .takeIf { it.isInt() }
+                ?.toInt()
+                ?: throw InterpreterException("Expected an integer index")
     }
 }
